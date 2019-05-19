e0fb77693d12b771b1b611a1612fb33eaf27e5e4
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 17:06:50 +0300

    Create a CastConverter class

---
+++ app/src/main/java/com/joe/preview/data/local/converters/CastConverter.java
@@ -0,0 +1,26 @@
+package com.joe.preview.data.local.converters;
+
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.joe.preview.data.remote.Cast;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class CastConverter {
+
+    @TypeConverter
+    public List<Cast> fromString(String value) {
+        Type listType = new TypeToken<List<Cast>>() {
+        }.getType();
+
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public String fromList(List<Cast> casts) {
+        return new Gson().toJson(casts);
+    }
+}
