06b03119bf86b8e441eb79a89efb7c26e97c2d51
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 17:12:04 +0300

    Create a GenreConverter class

---
+++ app/src/main/java/com/joe/preview/data/local/converters/GenreConverter.java
@@ -0,0 +1,27 @@
+package com.joe.preview.data.local.converters;
+
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.joe.preview.data.remote.Genre;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class GenreConverter {
+
+    @TypeConverter
+    public List<Genre> fromString(String value) {
+        Type listType = new TypeToken<List<Genre>>() {
+        }.getType();
+
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public String fromList(List<Genre> genres) {
+        return new Gson().toJson(genres);
+    }
+
+}
