93ed5a19218c978f4b08db01b9a114efc6da302f
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 18:42:38 +0300

    Create a NetworkInterceptor class

---
+++ app/src/main/java/com/joe/preview/data/remote/interceptors/NetworkInterceptor.java
@@ -0,0 +1,36 @@
+package com.joe.preview.data.remote.interceptors;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkInterceptor implements Interceptor {
+
+    private Context context;
+
+    public NetworkInterceptor(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        if (ConnectivityStatus.isConnected(context))
+            request.newBuilder()
+            .header("Cache-Control", "public, max-age=" + 60)
+            .build();
+        else
+            request.newBuilder()
+            .header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7)
+            .build();
+        return chain.proceed(request);
+    }
+
+}
