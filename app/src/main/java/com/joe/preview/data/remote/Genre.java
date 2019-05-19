a0b6ee3191441469b52b8f82230ca365b971d0ca
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 16:50:07 +0300

    Create a Genre class

---
+++ app/src/main/java/com/joe/preview/data/remote/Genre.java
@@ -0,0 +1,57 @@
+package com.joe.preview.data.remote;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Genre implements Parcelable {
+
+    private Long id;
+    private String name;
+
+    public Genre() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    protected Genre(Parcel in) {
+        this.id = (Long) in.readValue((Long.class.getClassLoader()));
+        this.name = in.readString();
+    }
+
+    public static final Creator<Genre> CREATOR = new Creator<Genre>() {
+        @Override
+        public Genre createFromParcel(Parcel in) {
+            return new Genre(in);
+        }
+
+        @Override
+        public Genre[] newArray(int size) {
+            return new Genre[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeValue(id);
+        parcel.writeString(name);
+    }
+}
