01ffc70ac2705f30a66db8f7d1ff4c1e5032aed1
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 18:46:53 +0300

    Create a RequestInterceptor class

---
+++ app/src/main/java/com/joe/preview/data/remote/interceptors/RequestInterceptor.java
@@ -0,0 +1,29 @@
+package com.joe.preview.data.remote.interceptors;
+
+import androidx.annotation.NonNull;
+
+import com.joe.preview.constants.PreviewConstants;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestInterceptor implements Interceptor {
+
+    @NonNull
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        HttpUrl httpUrl = request.url();
+        HttpUrl url = httpUrl.newBuilder()
+                .addQueryParameter("api_key", PreviewConstants.TMDB_API_KEY)
+                .build();
+        Request.Builder builder = request.newBuilder().url(url);
+        Request newRequest = builder.build();
+        return chain.proceed(newRequest);
+    }
+
+}
