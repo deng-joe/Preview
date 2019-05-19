0d9c1161801f1483e289978162cccfad9b9e0512
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 16:57:56 +0300

    Create a VideoResponse class

---
+++ app/src/main/java/com/joe/preview/data/remote/VideoResponse.java
@@ -0,0 +1,62 @@
+package com.joe.preview.data.remote;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoResponse implements Parcelable {
+
+    private long id;
+    private List<Video> results;
+
+    public VideoResponse() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Video> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Video> results) {
+        this.results = results;
+    }
+
+    protected VideoResponse(Parcel in) {
+        this.id = in.readLong();
+        this.results = new ArrayList<>();
+        in.readList(this.results, Video.class.getClassLoader());
+    }
+
+    public static final Creator<VideoResponse> CREATOR = new Creator<VideoResponse>() {
+        @Override
+        public VideoResponse createFromParcel(Parcel in) {
+            return new VideoResponse(in);
+        }
+
+        @Override
+        public VideoResponse[] newArray(int size) {
+            return new VideoResponse[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(this.id);
+        parcel.writeList(this.results);
+    }
+
+}
