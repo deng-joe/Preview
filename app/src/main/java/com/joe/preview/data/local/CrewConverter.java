198752ee6d81e693d1f9eb0d9036bb941e56e2ad
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 17:08:17 +0300

    Create a CrewConverter class

---
+++ app/src/main/java/com/joe/preview/data/local/CrewConverter.java
@@ -0,0 +1,27 @@
+package com.joe.preview.data.local;
+
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.joe.preview.data.remote.Crew;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class CrewConverter {
+
+    @TypeConverter
+    public List<Crew> fromString(String value) {
+        Type listType = new TypeToken<List<Crew>>() {
+        }.getType();
+
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public String fromList(List<Crew> crews) {
+        return new Gson().toJson(crews);
+    }
+
+}
