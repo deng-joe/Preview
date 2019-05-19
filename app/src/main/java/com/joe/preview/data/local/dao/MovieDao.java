c235ccf5944d6aad88a3d9bf9a812321fb78ec63
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 18:07:42 +0300

    Create a MovieDao interface

---
+++ app/src/main/java/com/joe/preview/data/local/dao/MovieDao.java
@@ -0,0 +1,31 @@
+package com.joe.preview.data.local.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.joe.preview.data.local.entity.Movie;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface MovieDao {
+
+    @Insert(onConflict = REPLACE)
+    void insertMovies(List<Movie> movies);
+
+    @Insert(onConflict = REPLACE)
+    void insertMovie(Movie movieEntity);
+
+    @Update(onConflict = REPLACE)
+    void updateMovie(Movie movie);
+
+    @Query("SELECT * FROM films WHERE id = :id")
+    Movie getMovieById(Long id);
+
+    @Query("SELECT * FROM films WHERE page = :page")
+    List<Movie> getMoviesByPage(Long page);
+}
