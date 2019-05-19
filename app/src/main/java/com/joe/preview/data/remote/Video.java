0ea01db40825e3cc7d9c194927310cd3126b11b1
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-19 16:54:44 +0300

    Create a Video class

---
+++ app/src/main/java/com/joe/preview/data/remote/Video.java
@@ -0,0 +1,58 @@
+package com.joe.preview.data.remote;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Video implements Parcelable {
+
+    private String id;
+    private String key;
+
+    public Video() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    protected Video(Parcel in) {
+        this.id = in.readString();
+        this.key = in.readString();
+    }
+
+    public static final Creator<Video> CREATOR = new Creator<Video>() {
+        @Override
+        public Video createFromParcel(Parcel in) {
+            return new Video(in);
+        }
+
+        @Override
+        public Video[] newArray(int size) {
+            return new Video[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.id);
+        parcel.writeString(this.key);
+    }
+
+}
