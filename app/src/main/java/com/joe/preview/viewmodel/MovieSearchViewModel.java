88caa6fbf4aaea3844dc2b60e169040991e8fcc7
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-20 22:04:53 +0300

    Create a MovieSearchViewModel class

---
+++ app/src/main/java/com/joe/preview/viewmodel/MovieSearchViewModel.java
@@ -0,0 +1,38 @@
+package com.joe.preview.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.joe.preview.data.local.dao.MovieDao;
+import com.joe.preview.data.local.entity.Movie;
+import com.joe.preview.data.remote.api.MovieApiService;
+import com.joe.preview.data.remote.resources.Resource;
+import com.joe.preview.data.repository.MovieRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class MovieSearchViewModel extends BaseViewModel {
+
+    private MovieRepository movieRepository;
+    private MutableLiveData<Resource<List<Movie>>> moviesLiveData = new MutableLiveData<>();
+
+    @Inject
+    public MovieSearchViewModel(MovieDao movieDao, MovieApiService movieApiService) {
+        movieRepository = new MovieRepository(movieDao, movieApiService);
+    }
+
+    public MutableLiveData<Resource<List<Movie>>> getMoviesLiveData() {
+        return moviesLiveData;
+    }
+
+    public void searchMovie(String text) {
+        movieRepository.searchMovies(1L, text)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(listResource -> getMoviesLiveData().postValue(listResource));
+    }
+}
