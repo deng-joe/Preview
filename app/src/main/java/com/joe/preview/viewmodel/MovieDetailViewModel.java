6ab11058f5ac35b6d8ff4e6b7fe128ba6ab41d67
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-20 21:56:02 +0300

    Create a MovieDetailViewModel class

---
+++ app/src/main/java/com/joe/preview/viewmodel/MovieDetailViewModel.java
@@ -0,0 +1,33 @@
+package com.joe.preview.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.joe.preview.data.local.dao.MovieDao;
+import com.joe.preview.data.local.entity.Movie;
+import com.joe.preview.data.remote.api.MovieApiService;
+import com.joe.preview.data.repository.MovieRepository;
+
+import javax.inject.Inject;
+
+public class MovieDetailViewModel extends BaseViewModel {
+
+    private MovieRepository movieRepository;
+    private MutableLiveData<Movie> movieDetailsLiveData = new MutableLiveData<>();
+
+    @Inject
+    public MovieDetailViewModel(MovieDao movieDao, MovieApiService movieApiService) {
+        movieRepository = new MovieRepository(movieDao, movieApiService);
+    }
+
+    public MutableLiveData<Movie> getMovieDetailsLiveData() {
+        return movieDetailsLiveData;
+    }
+
+    public void fetchMovieDetails(Movie movie) {
+        movieRepository.fetchMovieDetails(movie.getId())
+                .subscribe(movieResource -> {
+                    if (movieResource.isLoaded())
+                        getMovieDetailsLiveData().postValue(movieResource.data);
+                });
+    }
+}
