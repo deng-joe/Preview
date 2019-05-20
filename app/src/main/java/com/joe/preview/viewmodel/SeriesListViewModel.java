603e6462c8ad68c74ebfe021daf2d4b918830490
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-20 22:24:17 +0300

    Create a SeriesListViewModel class

---
+++ app/src/main/java/com/joe/preview/viewmodel/SeriesListViewModel.java
@@ -0,0 +1,45 @@
+package com.joe.preview.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.joe.preview.data.local.dao.SeriesDao;
+import com.joe.preview.data.local.entity.Series;
+import com.joe.preview.data.remote.api.SeriesApiService;
+import com.joe.preview.data.remote.resources.Resource;
+import com.joe.preview.data.repository.SeriesRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class SeriesListViewModel extends BaseViewModel {
+
+    private String type;
+    private SeriesRepository seriesRepository;
+    private MutableLiveData<Resource<List<Series>>> seriesLiveData = new MutableLiveData<>();
+
+    @Inject
+    public SeriesListViewModel(SeriesDao seriesDao, SeriesApiService seriesApiService) {
+        seriesRepository = new SeriesRepository(seriesDao, seriesApiService);
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public MutableLiveData<Resource<List<Series>>> getSeriesLiveData() {
+        return seriesLiveData;
+    }
+
+    public void loadMoreSeries(Long currentPage) {
+        seriesRepository.loadSeriesByType(currentPage, type)
+                .doOnSubscribe(disposable -> addToDisposable(disposable))
+                .subscribe(listResource -> getSeriesLiveData().postValue(listResource));
+    }
+
+    public boolean isLastPage() {
+        return (seriesLiveData.getValue() != null &&
+                !seriesLiveData.getValue().data.isEmpty()) && seriesLiveData.getValue().data.get(0)
+                .isLastPage();
+    }
+}
