faff3bd45d223d87feee5d09b9936bc5b118b09b
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-20 21:38:00 +0300

    Create a BaseViewModel class to be the super class for all the ViewModels

---
+++ app/src/main/java/com/joe/preview/viewmodel/BaseViewModel.java
@@ -0,0 +1,21 @@
+package com.joe.preview.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+public class BaseViewModel extends ViewModel {
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    protected void addToDisposable(Disposable disposable) {
+        compositeDisposable.remove(disposable);
+        compositeDisposable.add(disposable);
+    }
+
+    public void onStop() {
+        compositeDisposable.clear();
+    }
+
+}
