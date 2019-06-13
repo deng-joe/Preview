ac5835b92a375217fa8394a3ae40d800fac2424f
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-13 22:02:05 +0300

    Create a class to flip view when switching fragments

---
+++ app/src/main/java/com/joe/preview/ui/custom/FlipAnimation.java
@@ -0,0 +1,50 @@
+package com.joe.preview.ui.custom;
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class FlipAnimation extends Animation {
+
+    private float fromDegrees;
+    private float toDegrees;
+    private float centerX;
+    private float centerY;
+    private Camera camera;
+
+    public FlipAnimation(float fromDegrees, float toDegrees, float centerX, float centerY) {
+        this.fromDegrees = fromDegrees;
+        this.toDegrees = toDegrees;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+        super.initialize(width, height, parentWidth, parentHeight);
+        camera = new Camera();
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        float fromDegrees = this.fromDegrees;
+        float degrees = fromDegrees + ((toDegrees - fromDegrees) * interpolatedTime);
+
+        float centerX = this.centerX;
+        float centerY = this.centerY;
+
+        Camera camera = this.camera;
+
+        Matrix matrix = t.getMatrix();
+
+        camera.save();
+        camera.rotateY(degrees);
+        camera.getMatrix(matrix);
+        camera.restore();
+
+        matrix.postTranslate(-centerX, -centerY);
+        matrix.postTranslate(centerX, centerY);
+    }
+
+}
