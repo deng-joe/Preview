0a792959509391911dc0da5d0349cb819d1ce0a8
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-15 12:32:38 +0300

    Create adapters for displaying results when searching a movie or TV show

---
+++ app/src/main/java/com/joe/preview/adapter/MovieSearchListAdapter.java
@@ -0,0 +1,79 @@
+package com.joe.preview.adapter;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.joe.preview.R;
+import com.joe.preview.data.local.entity.Movie;
+import com.joe.preview.databinding.MoviesListItemBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieSearchListAdapter extends RecyclerView.Adapter<MovieSearchListAdapter.MovieSearchViewHolder> {
+
+    private Activity activity;
+    private List<Movie> trailers;
+
+    public MovieSearchListAdapter(Activity activity, List<Movie> trailers) {
+        this.activity = activity;
+        this.trailers = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public MovieSearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        MoviesListItemBinding itemBinding = MoviesListItemBinding.inflate(inflater, parent, false);
+        return new MovieSearchViewHolder(itemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieSearchViewHolder holder, int position) {
+        Movie trailer = getItem(position);
+        holder.bindTo(trailer);
+    }
+
+    @Override
+    public int getItemCount() {
+        return trailers.size();
+    }
+
+    public Movie getItem(int position) {
+        return trailers.get(position);
+    }
+
+    public void setItems(List<Movie> trailers) {
+        this.trailers = trailers;
+        notifyDataSetChanged();
+    }
+
+
+    class MovieSearchViewHolder extends RecyclerView.ViewHolder {
+
+        private MoviesListItemBinding binding;
+
+        MovieSearchViewHolder(MoviesListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            int width = displayMetrics.widthPixels;
+
+            itemView.setLayoutParams(new RecyclerView.LayoutParams(Float.valueOf(width * 0.85f).intValue(),
+                    RecyclerView.LayoutParams.WRAP_CONTENT));
+        }
+
+        private void bindTo(Movie trailer) {
+            Picasso.get().load(trailer.getPosterPath()).placeholder(R.drawable.ic_image).into(binding.image);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/joe/preview/adapter/SeriesSearchListAdapter.java
@@ -0,0 +1,79 @@
+package com.joe.preview.adapter;
+
+import android.app.Activity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.joe.preview.R;
+import com.joe.preview.data.local.entity.Series;
+import com.joe.preview.databinding.MoviesListItemBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeriesSearchListAdapter extends RecyclerView.Adapter<SeriesSearchListAdapter.SeriesSearchViewHolder> {
+
+    private Activity activity;
+    private List<Series> trailers;
+
+    public SeriesSearchListAdapter(Activity activity, List<Series> trailers) {
+        this.activity = activity;
+        this.trailers = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public SeriesSearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        MoviesListItemBinding itemBinding = MoviesListItemBinding.inflate(inflater, parent, false);
+        return new SeriesSearchViewHolder(itemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SeriesSearchViewHolder holder, int position) {
+        Series trailer = getItem(position);
+        holder.bindTo(trailer);
+    }
+
+    @Override
+    public int getItemCount() {
+        return trailers.size();
+    }
+
+    public Series getItem(int position) {
+        return trailers.get(position);
+    }
+
+    public void setTrailers(List<Series> trailers) {
+        this.trailers = trailers;
+        notifyDataSetChanged();
+    }
+
+
+    class SeriesSearchViewHolder extends RecyclerView.ViewHolder {
+
+        private MoviesListItemBinding binding;
+
+        public SeriesSearchViewHolder(MoviesListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            int width = displayMetrics.widthPixels;
+
+            itemView.setLayoutParams(new RecyclerView.LayoutParams(Float.valueOf(width * 0.85f).intValue(),
+                    RecyclerView.LayoutParams.WRAP_CONTENT));
+        }
+
+        private void bindTo(Series trailer) {
+            Picasso.get().load(trailer.getPosterPath()).placeholder(R.drawable.ic_image).into(binding.image);
+        }
+    }
+
+}
