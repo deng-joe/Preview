cc5312f5dc0ba7cf82461fbacaf24c5718a0c3a3
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-20 14:53:19 +0300

    Create a FragmentModule to generate injector for fragments

---
+++ app/src/main/java/com/joe/preview/di/FragmentModule.java
@@ -0,0 +1,18 @@
+package com.joe.preview.di;
+
+import com.joe.preview.ui.fragment.MoviesListFragment;
+import com.joe.preview.ui.fragment.SeriesListFragment;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class FragmentModule {
+
+    @ContributesAndroidInjector
+    abstract MoviesListFragment contributeMovieListFragment();
+
+    @ContributesAndroidInjector
+    abstract SeriesListFragment contributeSeriesListFragment();
+
+}
