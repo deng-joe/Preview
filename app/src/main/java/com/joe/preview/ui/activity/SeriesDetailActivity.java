3cbe0f1ee4f6a3d88980801092233a4e84eddc57
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-20 14:33:13 +0300

    Create an activity for showing TV shows details

---
+++ app/src/main/java/com/joe/preview/ui/activity/SeriesDetailActivity.java
@@ -0,0 +1,13 @@
+package com.joe.preview.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeriesDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
