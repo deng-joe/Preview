acd347c2cb4c660371b5753eb6c0c075e08612be
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-20 14:35:33 +0300

    Create an activity for searching movies

---
+++ app/src/main/java/com/joe/preview/ui/activity/MovieSearchActivity.java
@@ -0,0 +1,16 @@
+package com.joe.preview.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.joe.preview.R;
+
+public class MovieSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_search);
+    }
+}
