757ec23a919af755a158d7ee547b3c12a1bcaefe
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-20 14:59:02 +0300

    Create an ActivityModule class to generate AndroidInjector for activities defined in this class

---
+++ app/src/main/java/com/joe/preview/di/ActivityModule.java
@@ -0,0 +1,30 @@
+package com.joe.preview.di;
+
+import com.joe.preview.ui.activity.MainActivity;
+import com.joe.preview.ui.activity.MovieDetailActivity;
+import com.joe.preview.ui.activity.MovieSearchActivity;
+import com.joe.preview.ui.activity.SeriesDetailActivity;
+import com.joe.preview.ui.activity.SeriesSearchActivity;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityModule {
+
+    @ContributesAndroidInjector(modules = FragmentModule.class)
+    abstract MainActivity contributeMainActivity();
+
+    @ContributesAndroidInjector()
+    abstract MovieDetailActivity contributeMovieDetailActivity();
+
+    @ContributesAndroidInjector()
+    abstract SeriesDetailActivity contributeSeriesDetailActivity();
+
+    @ContributesAndroidInjector()
+    abstract MovieSearchActivity contributeMovieSearchActivity();
+
+    @ContributesAndroidInjector()
+    abstract SeriesSearchActivity contributeSeriesSearchActivity();
+
+}
